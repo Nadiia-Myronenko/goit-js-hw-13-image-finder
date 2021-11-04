b1d05d01ddc2251d7586438912e10d11548085ca
Nadiia-Myronenko <nadegda.burina@gmail.com>
2021-11-04 18:30:08 +0200

    Сделан рендеринг

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+import articlesTpl from './templates/articles.hbs';
+import NewsApiService from './apiService';
 import './sass/styles.scss';
+
+
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallary: document.querySelector('.gallery'),
+    loadMoreButton: document.querySelector('[data-action="load-more"]')
+};
+
+const newsApiService = new NewsApiService();
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreButton.addEventListener('click', onLoadMore);
+
+
+
+function onSearch(e) {
+    e.preventDefault();
+    newsApiService.resetPage();
+    newsApiService.query = e.currentTarget.elements.query.value;
+    newsApiService.fetchArticles().then(appendArticlesMarkup);
+}
+function onLoadMore() {
+    newsApiService.fetchArticles().then(appendArticlesMarkup);
+}
+function appendArticlesMarkup(articles) {
+    refs.gallary.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
